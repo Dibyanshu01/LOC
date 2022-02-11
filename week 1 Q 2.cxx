6b1b9814ca154e4cb0bad27c92e46c5a75782237
Dibyanshu01 <99402613+Dibyanshu01@users.noreply.github.com>
2022-02-11 13:28:51 +0530

    Add files via upload

---
+++ week 1 Q 2.cxx
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+ 
+{
+	int a,b;
+	printf("enter two numbers");
+	scanf("%d%d",&a,&b);
+	if(a-b<0)
+	{
+	    printf("b is maximum");
+	}
+	else
+	{
+	    printf("a is maxumum");
+	    }
+	    return 0;
+}
