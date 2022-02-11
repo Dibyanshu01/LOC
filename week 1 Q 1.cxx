d4c25df7fdcb3db5324226bfdefcdfdb8b0b59ec
Dibyanshu01 <99402613+Dibyanshu01@users.noreply.github.com>
2022-02-11 14:30:30 +0530

    Add files via upload

---
+++ week 1 Q 1.cxx
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+
+{
+	char name[20],branch[20],reg[20];
+	printf("Enter your name:\n");
+	scanf("%s",&name);
+	printf("Enter your branch:\n");
+	scanf("%s",&branch);
+	printf("Enter your registration number:\n");
+	scanf("%s",&reg);
+	printf("\nYour name is %s",name);
+	printf("\nYour branch is %s",branch);
+	printf("\nYour registration number is %s",reg);   
+	
+}
